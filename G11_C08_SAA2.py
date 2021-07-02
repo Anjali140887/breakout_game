ebedbbe5383b8b6ef7d1eebe9bc38aba10b4a7aa
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-07-02 12:49:29 +0530

    Add files via upload

---
+++ G11_C08_SAA2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 12:45:50 2021
+
+@author: ANJALI SHARMA
+"""
+
+num_list = [10, 20, 30, 40]
+
+sum = 0
+
+for val in num_list:
+    sum = sum+val
+
+print("The sum is", sum)
