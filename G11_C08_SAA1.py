d76a56cab7deec6c4834a22024a16219b80a419c
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-07-02 12:42:43 +0530

    Add files via upload

---
+++ G11_C08_SAA1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 12:39:30 2021
+
+@author: ANJALI SHARMA
+"""
+
+
+n = 10
+
+sum = 0 #Tracking the sum
+i = 1 #Tracking the counter
+
+while i <= n:
+    sum = sum + i
+    i = i+1
+
+
+print("The sum is", sum)
